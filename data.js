446d65e0825c4686698813f78e16256e839366eb
David Schumann <david@schumann.com>
2026-02-16 00:29:37 +0100

    Version 1.0 of shop

--- data.js
+++ data.js
@@ -13,20 +13,21 @@ const SHOP_DATA = [
         name: "Accessories", 
         prefix: "saf", 
         items: [
-            { name: "Tech Visor", price: "$45", link: "#", img: "products/acc1.jpg" },
-            { name: "Utility Belt", price: "$60", link: "#", img: "products/acc2.jpg" },
-            { name: "Pulse Link", price: "$30", link: "#", img: "products/acc3.jpg" },
-            { name: "Scout Mask", price: "$55", link: "#", img: "products/acc4.jpg" }
+            { name: "Tech Visor", price: "$45", link: "#", img: "products/acc1.png" },
+            { name: "Utility Belt", price: "$60", link: "#", img: "products/acc2.png" },
+            { name: "Pulse Link", price: "$30", link: "#", img: "products/acc3.png" },
+            { name: "Scout Mask", price: "$55", link: "#", img: "products/acc4.png" }
         ] 
     },
     { 
         name: "Silver", 
         prefix: "dav", 
         items: [
-            { name: "Heavy Ring", price: "$150", link: "#", img: "products/silver1.jpg" },
-            { name: "Silver Chain", price: "$210", link: "#", img: "products/silver2.jpg" },
-            { name: "Sigil Studs", price: "$90", link: "#", img: "products/silver3.jpg" },
-            { name: "Link Bracelet", price: "$180", link: "#", img: "products/silver4.jpg" }
+            { name: "Silver Chain L", price: "$150", link: "#", img: "products/silver1.jpg" },
+            { name: "Heart Bracelet M", price: "$210", link: "#", img: "products/silver2.jpg" },
+            { name: "Silver Chain XL", price: "$90", link: "#", img: "products/silver3.jpg" },
+            { name: "Link Bracelet L", price: "$180", link: "#", img: "products/silver4.jpg" },
+            { name: "Barbed Wire Bracelet", price: "$180", link: "#", img: "products/silver5.jpg" }
         ] 
     }
 ];
